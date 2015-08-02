ca54cbe35b81be06c1012e9b1ca72f36707b7d73
StevenLi98 <Spidey@Stevens-MacBook-Pro.local>
2015-08-02 14:53:22 -0400

    choke change

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
-   
+
    $('div').not("#home").hide();
    $("#home button").click(function(){
       $("#home").fadeOut("slow", function(){
          $("#letter").fadeIn();
       });
    });
-   
+
    $("#letter h5").click(function(){
       $("#letter").fadeOut("slow", function(){
          $("#prison").fadeIn();
       });
    });
-   
+
    $("#prison img[name=spoon], img[name=tray]").click(function(){
       var name = $(this).attr("name");
       $("#prison").fadeOut("slow", function(){
          $("#" + name + "-choice").fadeIn();
       });
    });
-   
+
    $("#spoon-choice a").click(function(){
       var name = $(this).attr("name");
-      
+
       $("#spoon-choice").fadeOut("slow", function(){
          if(name==='use')
          {
@@ -35,13 +35,13 @@ $(document).ready(function(){
             $("#prison").fadeIn();
       })
    });
-   
+
    $("#tray-choice a").click(function(){
       var name = $(this).attr("name");
       $("#tray-choice").fadeOut("slow", function(){
          if(name === "rubberband")
          {
-            $("#gameover").append("<h1>You choked on the rubber band.</h1>");
+            $("#gameover").append("<h1>The rubber band hit the guard in the face.</h1>");
             $("#gameover").append("<img src='images/choking.png'>");
             $("#gameover").append("<h1 name='startover'>Start Over!</h1>");
             $("#gameover").fadeIn();
@@ -50,7 +50,7 @@ $(document).ready(function(){
             $("#" + name).fadeIn();
       });
    });
-   
+
    $("#diamond a").click(function(){
       var name = $(this).attr("name");
       $("#diamond").fadeOut("slow", function(){
@@ -64,31 +64,31 @@ $(document).ready(function(){
             $("#" + name).fadeIn();
       });
    });
-   
+
    $("h1[name=jailfree]").click(function(){
       $("#police").fadeOut("slow", function(){
          $("#jailfree").fadeIn();
       });
    });
-   
+
    $("h1[name=win]").click(function(){
       $("#jailfree").fadeOut("slow", function(){
          $("#win").fadeIn();
       });
    });
-   
+
    $("#paperclip img").click(function(){
       $("#paperclip").fadeOut("slow", function(){
          $("#unlocked").fadeIn();
       });
    });
-   
+
    $("#unlocked img").click(function(){
       $("#unlocked").fadeOut("slow", function(){
          $("#win").fadeIn();
       });
    });
-   
+
    $(document).on("click", "h1[name=startover]", function(){
       $("#gameover").fadeOut("slow", function(){
          $("#home").fadeIn();
